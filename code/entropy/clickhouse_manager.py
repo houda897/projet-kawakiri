93d91785e9671e125cb8a925d9a4e0fbbf1749bf
Maxime BERGER <maxime.berger@etu.univ-amu.fr>
2026-05-12 16:59:16 +0200

    feat : clickhouse connection is now controller by the .env

--- code/entropy/clickhouse_manager.py
+++ code/entropy/clickhouse_manager.py
@@ -8,40 +8,44 @@ from pathlib import Path
 class clickhouse_manager :
     """ This class connects the client to the clickhouse server """
 
-    def __init__ (self) :
-        '''Client initialization'''
+    _instance = None
+
+    def __init__(self):
         
-        env_path = Path(__file__).with_name(".env")
-        load_dotenv(dotenv_path=env_path)
+        current_file_path = os.path.abspath(__file__)
+        root_dir = os.path.dirname(os.path.dirname(os.path.dirname(current_file_path)))
+        env_path = os.path.join(root_dir, ".env")
+        
+        if os.path.exists(env_path):
+            load_dotenv(dotenv_path=env_path)
+        else:
+            print(f"Error, .env not found {env_path}")
 
-        self.CH_HOST = os.getenv("CH_HOST", "54.36.175.8")
-        #print("self.CH_HOST", self.CH_HOST)
-        self.CH_PORT = int(os.getenv("CH_PORT", "19123"))
-        #print("self.CH_PORT", self.CH_PORT)
-        self.CH_USER = os.getenv("CH_USER", "lab_admin")
-        #print("self.CH_USER", self.CH_USER)
-        self.CH_PASSWORD = os.getenv("CH_PASSWORD", "S!7mZ@9w#LpT3eXq")
-        #print("self.CH_PASSWORD", self.CH_PASSWORD)
-        self.CH_DATABASE = os.getenv("CH_DATABASE", "default")
-        #print("self.CH_DATABASE", self.CH_DATABASE)
-
-
+        self.CH_HOST = os.getenv("CH_HOST") 
+        raw_port = os.getenv("CH_PORT")
+        self.CH_PORT = int(raw_port) if raw_port else 19123
+        self.CH_USER = os.getenv("CH_USER")
+        self.CH_PASSWORD = os.getenv("CH_PASSWORD")
+        self.CH_DATABASE = os.getenv("CH_DATABASE")
+        
         self.client = None
         self.connect()
 
     def connect(self) :
         '''Connects the client to the database'''
-        try:
-            self.client = clickhouse_connect.get_client(
-                host=self.CH_HOST,
-                port=self.CH_PORT,
-                username=self.CH_USER,
-                password=self.CH_PASSWORD
-            )
-            print("Clickhouse sucessfully connected.")
-        except Exception as e:
-            print(f"Error during clickhouse connection : {e}")
-            exit()
+        
+        if self.client is None:
+            try:
+                self.client = clickhouse_connect.get_client(
+                    host=self.CH_HOST,
+                    port=self.CH_PORT,
+                    username=self.CH_USER,
+                    password=self.CH_PASSWORD
+                )
+                print("Clickhouse sucessfully connected.")
+            except Exception as e:
+                print(f"Error during clickhouse connection : {e}")
+                exit()
 
     def query(self, sql):
         '''Executes a request and return the corresponding data'''
@@ -52,4 +56,13 @@ class clickhouse_manager :
         return self.client.query_df(sql)
 
     def get_CH_DB(self) :
+        '''Getter'''
         return self.CH_DATABASE
+    
+    @classmethod
+    def get_client(cls):
+        '''Static method to get the client from everywhere'''
+        if cls._instance is None:
+            cls._instance = cls()
+            cls._instance.connect()
+        return cls._instance.client
