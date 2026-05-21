0078f6c50a6d317bb2f0b52c9b8433d49d413120
Maxime <maxime.berger@etu.univ-amu.fr>
2026-05-21 13:22:18 +0200

    fix : greedy algorithme to generate composite key

--- code/inference/composite_key.py
+++ code/inference/composite_key.py
@@ -3,6 +3,7 @@ from core.logger import get_logger
 from inference.primary_key import PrimaryKeyCandidate
 from core.clickhouse_manager import clickhouse_manager
 from collections import defaultdict
+from stats.functional_dependency import check_functional_dependency
 
 logger = get_logger(__name__)
 
@@ -14,10 +15,14 @@ class CompositeKeyEngine:
         """Check if a ClickHouse type is numeric."""
         t = ch_type.lower()
         return "int" in t or "float" in t or "decimal" in t
-
-    def generate_composite_candidates(self, all_columns: list[PrimaryKeyCandidate], tables_without_pk: list[str], max_size : int) -> list[PrimaryKeyCandidate]:
-        """Generate composite key candidates for tables without primary keys by using columns with the highest confidence score. """
-        composite_candidates = []
+    
+    def generate_composite_candidates(self, all_columns: list[PrimaryKeyCandidate], tables_without_pk: list[str]) -> list[PrimaryKeyCandidate]:
+        '''
+        Generates composite key candidates using a greedy iterative approach.
+        For each table without a simple PK, it starts with the column with the highest confidence score and iteratively adds columns based on their confidence and uniqueness ratios.
+        '''
+        
+        valid_composite_candidates = []
 
         columns_by_table = defaultdict(list)
         for c in all_columns:
@@ -30,35 +35,38 @@ class CompositeKeyEngine:
                 continue
 
             table_cols.sort(key=lambda c: (c.confidence, c.uniqueness_ratio), reverse=True)
+            
+            database_name = table_cols[0].database_name
 
-            all_combos = []
-            for size in range(2, max_size + 1):
-                for combo in itertools.combinations(table_cols, size):
-                    all_combos.append(list(combo))
-
-            if not all_combos:
-                continue
-
-            all_combos.sort(key=lambda combo: (
-                len(combo),
-                -sum(1 for c in combo if self._is_numeric(c.column_type))
-            ))
-
-            for best_combo in all_combos[:2]: 
-                combo_names = [c.column_name for c in best_combo]
+            current_combo = [table_cols[0], table_cols[1]]
+            
+            for size in range(2, len(table_cols) + 1):
                 
-                composite_candidates.append(PrimaryKeyCandidate(
-                    database_name=best_combo[0].database_name,
-                    table_name=table,
-                    column_name=", ".join(combo_names), 
-                    column_type="Composite",
-                    rows=best_combo[0].rows,
-                    null_ratio=0.0,
-                    uniqueness_ratio=1.0,
-                    entropy_ratio=sum(c.entropy_ratio for c in best_combo) / len(best_combo),
-                    identifiability_score=sum(c.identifiability_score for c in best_combo) / len(best_combo),
-                    confidence=sum(c.confidence for c in best_combo) / len(best_combo),
-                    reason=f"Composite Candidate (Size {len(combo_names)}) from top scores"
-                ))
-
-        return composite_candidates
+                if size > 2:
+                    current_combo.append(table_cols[size - 1])
+                
+                combo_names = [c.column_name for c in current_combo]
+                print(f"Testing combo for {table} (Size {size}): {combo_names}")
+                
+                is_valid = check_functional_dependency(database_name, table, combo_names, self.db)
+                
+                if is_valid:
+                    logger.info(f"✅ Clé composite trouvée pour {table} : {combo_names}")
+                    
+                    valid_composite_candidates.append(PrimaryKeyCandidate(
+                        database_name=database_name,
+                        table_name=table,
+                        column_name=", ".join(combo_names), 
+                        column_type="Composite",
+                        rows=current_combo[0].rows,
+                        null_ratio=0.0,
+                        uniqueness_ratio=1.0, 
+                        entropy_ratio=sum(c.entropy_ratio for c in current_combo) / len(current_combo),
+                        identifiability_score=sum(c.identifiability_score for c in current_combo) / len(current_combo),
+                        confidence=sum(c.confidence for c in current_combo) / len(current_combo),
+                        reason=f"Composite PK (Size {len(combo_names)}) validated by DF"
+                    ))
+                    
+                    break 
+                    
+        return valid_composite_candidates
